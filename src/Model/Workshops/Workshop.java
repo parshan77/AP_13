45bcaad6f237a86be144560fa3daf8665d7f6852
Parshan <parshanteimouri@yahoo.com>
2018-12-08 23:56:52 +0330

    Workshop -> Generic class!

--- src/Model/Workshops/Workshop.java
+++ src/Model/Workshops/Workshop.java
@@ -2,20 +2,18 @@ package Model.Workshops;
 
 import Model.Products.ProcessedProduct;
 import Model.Products.RawProduct;
+import Model.User;
 
 import java.util.ArrayList;
 
-public abstract class Workshop {
+public abstract class Workshop<T extends RawProduct,V extends ProcessedProduct> {
     private int level;
+    private User user;
+    private String name;
 
-    public int getLevel() {
-        return level;
-    }
-
-    private int activationPrice;
-
-    public int getActivationPrice() {
-        return activationPrice;
+    public Workshop(User user, String name) {
+        this.user = user;
+        this.name = name;
     }
 
     private RawProduct input;
