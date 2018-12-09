a929818e9fc388c3475a0b8f93b84fe44e4c516a
abolfazlghanbari23 <abolfazlghanbariaaaaaaaaaaaa@gmail.com>
2018-12-09 13:05:50 +0330

    WorkShop edited.

--- src/Model/Workshops/EggPowderPlant.java
+++ src/Model/Workshops/EggPowderPlant.java
@@ -1,4 +1,25 @@
 package Model.Workshops;
 
+import Model.Products.ProcessedProducts.Flour;
+import Model.Products.RawProducts.Egg;
+import Model.Warehouse;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class EggPowderPlant extends Workshop {
+
+    public EggPowderPlant(Warehouse warehouse, String name) {
+        super(warehouse, name);
+    }
+
+    public ArrayList<Flour>  process(ArrayList<Egg> Eggs){
+        if(isMoneyEnought){
+            ArrayList<Flour> Flours = new ArrayList<>();
+            //TODO :  process bayad takmil beshe.aslan be ezaye chand ta Egg,chand ta Flour mide?
+            return Flours;
+        }
+        else
+            return null;
+    }
 }
