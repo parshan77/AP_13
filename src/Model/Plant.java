04dd2b3618f0f9faead6adb74418ea8aee0b3154
Parshan <parshanteimouri@yahoo.com>
2018-12-14 17:13:21 +0330

    health attribute added to Plant class.

--- src/Model/Plant.java
+++ src/Model/Plant.java
@@ -1,11 +1,25 @@
 package Model;
 
+import Exceptions.PlantNotEnoughHealthException;
 import Interfaces.Visible;
 
 public class Plant implements Visible {
+    private double health = 100;
+
+    //constants
+    public static int PLANTING_NEEDED_WATER = 10;
+
+    public Plant() {
+    }
+
+    public void decreaseHealth(double amount) throws PlantNotEnoughHealthException{
+        if (health < amount)
+            throw new PlantNotEnoughHealthException();
+        health -= amount;
+    }
 
     @Override
     public void show() {
-
+        //todo
     }
 }
