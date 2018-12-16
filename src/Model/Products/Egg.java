b18e51329ee8eb16d15d8e0f74a25e2cbf7932de
abolfazlghanbari23 <abolfazlghanbariaaaaaaaaaaaa@gmail.com>
2018-12-16 19:48:35 +0330

    Egg

--- src/Model/Products/Egg.java
+++ src/Model/Products/Egg.java
@@ -1,13 +1,15 @@
 package Model.Products;
 
+import Model.Position;
+
 public class Egg extends Product {
     public static int volume = 1;
     public static int buyCost = 20;
     public static int sellCost = 10;
     public static String name ="Egg" ;
 
-    public Egg() {
-
+    public Egg(Position position) {
+        super(position);
     }
 
     @Override
