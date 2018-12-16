1c96ce50d97641ef926b3a6b49a9ce6f70ebdeaf
abolfazlghanbari23 <abolfazlghanbariaaaaaaaaaaaa@gmail.com>
2018-12-16 18:41:18 +0330

    Cloth & Dress & Fiber

---
+++ src/Model/Products/Dress.java
@@ -0,0 +1,8 @@
+package Model.Products;
+
+public class Dress extends Product {
+    private static int volume = 8;
+    private static int buyCost = 1000;
+    private static int sellCost = 500;
+    private static String name = "Dress";
+}
