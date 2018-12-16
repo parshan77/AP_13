925513164eb6545f1c9bdcd223950c552c79ae6a
abolfazlghanbari23 <abolfazlghanbariaaaaaaaaaaaa@gmail.com>
2018-12-16 19:06:00 +0330

    NoThingToSell

--- src/Model/Shop.java
+++ src/Model/Shop.java
@@ -1,21 +1,24 @@
 package Model;
 
-import Interfaces.VisibleInMap;
+import Interfaces.Visible;
 
 import java.util.ArrayList;
 
-public class Shop {
+public class Shop implements Visible {
     private ArrayList things;
 
     public ArrayList getThings() {
         return things;
     }
 
-    public void buy(String name) {
+    @Override
+    public void show(){
+    }
+    public void buy(String name){
 
     }
+    public void sell(Object object){
 
-    public void sell(Object object) {
     }
 
 }
