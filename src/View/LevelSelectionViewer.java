f96d953e581104e28ec5bb6ed522bbf97666985b
Abolfazl Ghanbari <ghanbarishce@ce.sharif.edu>
2019-02-02 23:18:28 +0330

    some changes in levelSelectionViewer

--- src/View/LevelSelectionViewer.java
+++ src/View/LevelSelectionViewer.java
@@ -17,118 +17,152 @@ import static View.MenuView.buildImageView;
 
 public class LevelSelectionViewer {
 
+    private ImageView backGround;
+    private ImageView levelsView;
+    private ImageView level1View;
+    private ImageView level2View;
+    private ImageView level3View;
+    private ImageView level4View;
+    private ImageView menuView;
+    private ImageView shopView;
+    Group root = new Group();
+    Stage primaryStage;
+
     boolean baz = true;
 
     public void start(Stage primaryStage) throws Exception {
-        Group root = new Group();
         Scene missionScene = new Scene(root);
         primaryStage.setScene(missionScene);
 
         primaryStage.setFullScreen(true);
         primaryStage.show();
 
-        ImageView level = buildImageView(root, "File:Textures\\MenuResources\\missionView2.jpg",
+        showBackground();
+        showShopButton();
+        showMenuButton();
+        showLevel1Button();
+        showLevel2Button();
+        showLevel3Button();
+        showLevel4Button();
+        showLevelsButton();
+
+    }
+
+    public void showBackground() {
+        backGround = buildImageView(root, "File:Textures\\MenuResources\\missionView2.jpg",
                 0, 0, primaryStage.getWidth(), primaryStage.getHeight(), true);
 
-        ImageView shop = buildImageView(root, "File:Textures\\MenuResources\\shop.png",
-                primaryStage.getWidth() * 0.03, primaryStage.getHeight() * 0.9,
-                150, 70, true);
-        shop.setOnMouseEntered(event -> {
-            becomeSmaller(shop);
-        });
-        shop.setOnMouseExited(event -> {
-            becomeBigger(shop);
-        });
+    }
 
-        ImageView menu = buildImageView(root, "File:Textures\\MenuResources\\menu.png",
-                primaryStage.getWidth() * 0.9, primaryStage.getHeight() * 0.9,
-                150, 70, true);
-        menu.setOnMouseEntered(event -> {
-            becomeSmaller(menu);
-        });
-        menu.setOnMouseExited(event -> {
-            becomeBigger(menu);
-        });
-
-        ImageView level1 = buildImageView(root, "File:Textures\\MenuResources\\level1.png",
-                0, 0, 100, 100, false);
-        level1.setOnMouseEntered(event -> {
-
-        });
-        level1.setOnMouseExited(event -> {
-
-        });
-
-        ImageView level2 = buildImageView(root, "File:Textures\\MenuResources\\level2.png",
-                0, 0, 100, 100, false);
-        level2.setOnMouseEntered(event -> {
-
-        });
-        level2.setOnMouseExited(event -> {
-
-        });
-
-        ImageView level3 = buildImageView(root, "File:Textures\\MenuResources\\level3.png",
-                0, 0, 100, 100, false);
-        level3.setOnMouseEntered(event -> {
-
-        });
-        level3.setOnMouseExited(event -> {
-
-        });
-
-        ImageView level4 = buildImageView(root, "File:Textures\\MenuResources\\level4.png",
-                0, 0, 100, 100, false);
-        level4.setOnMouseEntered(event -> {
-
-        });
-        level4.setOnMouseExited(event -> {
-
-        });
-
-        ImageView levels = buildImageView(root, "File:Textures\\MenuResources\\levels.png",
+    public void showLevelsButton(){
+        levelsView = buildImageView(root, "File:Textures\\MenuResources\\levels.png",
                 250, 450, 100, 100, true);
-        levels.setOnMouseClicked(event -> {
+
+        levelsView.setOnMouseClicked(event -> {
             if (baz) {
                 new PathTransition(
                         Duration.millis(1000),
                         new Path(new MoveTo(300, 500), new LineTo(550, 250)),
-                        level1
+                        level1View
                 ).play();
 
                 new PathTransition(
                         Duration.millis(1000),
                         new Path(new MoveTo(300, 500), new LineTo(550, 400)),
-                        level2
+                        level2View
                 ).play();
 
                 new PathTransition(
                         Duration.millis(1000),
                         new Path(new MoveTo(300, 500), new LineTo(550, 550)),
-                        level3
+                        level3View
                 ).play();
 
                 new PathTransition(
                         Duration.millis(1000),
                         new Path(new MoveTo(300, 500), new LineTo(550, 700)),
-                        level4
+                        level4View
                 ).play();
 
-                level1.setVisible(true);
-                level2.setVisible(true);
-                level3.setVisible(true);
-                level4.setVisible(true);
+                level1View.setVisible(true);
+                level2View.setVisible(true);
+                level3View.setVisible(true);
+                level4View.setVisible(true);
 
                 baz = false;
             } else {
-                level1.setVisible(false);
-                level2.setVisible(false);
-                level3.setVisible(false);
-                level4.setVisible(false);
+                level1View.setVisible(false);
+                level2View.setVisible(false);
+                level3View.setVisible(false);
+                level4View.setVisible(false);
 
                 baz = true;
             }
 
+        });
+    }
+    public void showLevel1Button() {
+        level1View = buildImageView(root, "File:Textures\\MenuResources\\level1.png",
+                0, 0, 100, 100, false);
+        level1View.setOnMouseEntered(event -> {
+
+        });
+        level1View.setOnMouseExited(event -> {
+
+        });
+    }
+
+    public void showLevel2Button() {
+        level2View = buildImageView(root, "File:Textures\\MenuResources\\level2.png",
+                0, 0, 100, 100, false);
+        level2View.setOnMouseEntered(event -> {
+
+        });
+        level2View.setOnMouseExited(event -> {
+
+        });
+    }
+    public void showLevel3Button() {
+        level3View = buildImageView(root, "File:Textures\\MenuResources\\level3.png",
+                0, 0, 100, 100, false);
+        level3View.setOnMouseEntered(event -> {
+
+        });
+        level3View.setOnMouseExited(event -> {
+
+        });
+    }
+    public void showLevel4Button() {
+        level4View = buildImageView(root, "File:Textures\\MenuResources\\level4.png",
+                0, 0, 100, 100, false);
+        level4View.setOnMouseEntered(event -> {
+
+        });
+        level4View.setOnMouseExited(event -> {
+
+        });
+    }
+
+    public void showShopButton() {
+        shopView = buildImageView(root, "File:Textures\\MenuResources\\shop.png",
+                primaryStage.getWidth() * 0.03, primaryStage.getHeight() * 0.9,
+                150, 70, true);
+        shopView.setOnMouseEntered(event -> {
+            becomeSmaller(shopView);
+        });
+        shopView.setOnMouseExited(event -> {
+            becomeBigger(shopView);
+        });
+    }
+    public void showMenuButton() {
+        menuView = buildImageView(root, "File:Textures\\MenuResources\\menu.png",
+                primaryStage.getWidth() * 0.9, primaryStage.getHeight() * 0.9,
+                150, 70, true);
+        menuView.setOnMouseEntered(event -> {
+            becomeSmaller(menuView);
+        });
+        menuView.setOnMouseExited(event -> {
+            becomeBigger(menuView);
         });
     }
 }
